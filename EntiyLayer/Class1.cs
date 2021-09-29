849d2263b9e320feebc5a72e0c2c300d6fea1f73
Ufuk91 <ufukbesir45@gmail.com>
2021-09-29 12:41:38 +0300

    19. Ders Tamamlandı

---
+++ EntiyLayer/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntiyLayer
+{
+    public class Class1
+    {
+    }
+}
