a8882d5f90b18d3bab03ce67a910d539461a46d3
Ufuk91 <ufukbesir45@gmail.com>
2021-10-05 22:50:12 +0300

    36. Ders Tamamlandı

--- MvcKampıBaşlıyor/Controllers/AdminCategoryController.cs
+++ MvcKampıBaşlıyor/Controllers/AdminCategoryController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DateAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +12,38 @@ namespace MvcKampıBaşlıyor.Controllers
 {
     public class AdminCategoryController : Controller
     {
-        // GET: AdminCategory
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
         public ActionResult Index()
         {
+            var categoryvalues = cm.Getlist();
+            return View(categoryvalues);
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            CategoryValidator categoryvalidator = new CategoryValidator();
+            ValidationResult result = categoryvalidator.Validate(p);
+            if(result.IsValid)
+            {
+                cm.CategoryAdd(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+
+                }
+                
+
+                
+            }
             return View();
         }
     }
