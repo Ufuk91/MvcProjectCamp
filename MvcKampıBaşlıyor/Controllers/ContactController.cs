7764692c3e8363b878aa353daf51f486a91179db
Ufuk91 <ufukbesir45@gmail.com>
2021-10-28 18:47:34 +0300

    60. Ders Tamamlandı

--- MvcKampıBaşlıyor/Controllers/ContactController.cs
+++ MvcKampıBaşlıyor/Controllers/ContactController.cs
@@ -14,12 +14,14 @@ namespace MvcKampıBaşlıyor.Controllers
         // GET: Contact
         ContactManager cm = new ContactManager(new EfContactDal());
         ContactValidator cv = new ContactValidator();
+        
         public ActionResult Index()
         {
             var contactvalues = cm.GetList();
 
             return View(contactvalues);
         }
+        
         public ActionResult GetContactDetails(int id)
         {
             var contactvalues = cm.GetByID(id);
